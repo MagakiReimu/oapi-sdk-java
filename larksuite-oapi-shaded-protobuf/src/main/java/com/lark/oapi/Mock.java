38827fd7fcd85f265781a20117dbad7cf4e8a80c
江以臣 <jiangyichen.maemual@bytedance.com>
2024-12-19 14:07:52 +0800

    fix protobuf

---
+++ larksuite-oapi-shaded-protobuf/src/main/java/com/lark/oapi/Mock.java
@@ -0,0 +1,14 @@
+package com.lark.oapi;
+
+/**
+ * This is a mock Java application.
+ */
+public class Mock {
+    /**
+     * Main method that prints a greeting.
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
